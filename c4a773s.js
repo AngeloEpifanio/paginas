75e2c6dabdbc07b36b3e885a561c2f7a88de40fd
Angelo Epifanio <42416985+AngeloEpifanio@users.noreply.github.com>
2021-05-05 17:01:58 -0300

    Add files via upload

--- c4a773s.js
+++ c4a773s.js
@@ -1,6 +1,3 @@
-<div id="lhc_status_container_page" ></div>
-
-<!-- Coloque esta tag depois da tag de status do Live Helper Chat. -->
 <script>var LHC_API = LHC_API||{};
 LHC_API.args = {mode:'embed',lhc_base_url:'//alosaude.com.br/atendimento/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'idealsaude.com.br',fresh:true,leaveamessage:true,department:[1],check_messages:false,lang:'por/',position_placement:'full_height_right'};
 (function() {
