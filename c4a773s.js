d640fa044d2c59057ae3e2efd43615047a01c3a5
Angelo Epifanio <42416985+AngeloEpifanio@users.noreply.github.com>
2021-05-05 23:42:37 -0300

    Add files via upload

---
+++ c4a773s.js
@@ -0,0 +1,7 @@
+var LHC_API = LHC_API||{};
+LHC_API.args = {mode:'embed',lhc_base_url:'//alosaude.com.br/atendimento/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'idealsaude.com.br',fresh:true,leaveamessage:true,department:[1],check_messages:false,lang:'por/',position_placement:'full_height_right'};
+(function() {
+var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+var date = new Date();po.src = '//alosaude.com.br/atendimento/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
